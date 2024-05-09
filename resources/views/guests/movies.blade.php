80b90a9ffc1eeda0691ef7e6cba1af5b08c8dd1f
Kasketto001 <kaskettomaledetto@gmail.com>
2024-05-09 17:56:02 +0200

    Restyle the cards

--- resources/views/guests/movies.blade.php
+++ resources/views/guests/movies.blade.php
@@ -1,24 +1,29 @@
 @extends('layouts.app')
 
-
 @section('content')
-
-@forelse ($movies as $movie )
-
-        <div class="col">
-            <div class="card">
+<div class="container mt-5">
+    <div class="row">
+        @forelse ($movies as $movie)
+        <div class="col-md-4 mb-4">
+            <div class="card card-custom">
+                <div class="card-header">
+                    <h5 class="card-title">{{$movie->title}}</h5>
+                    <p class="card-text"><strong>Original Title:</strong> {{$movie->original_title}}</p>
+                </div>
                 <div class="card-body">
-                    <h3 class="card-title">
-                        {{$movie->title}}
-                    </h3>
-                    <p>{{$movie->original_title}}</p>
+                    <p class="card-text"><strong>Nationality:</strong> {{$movie->nationality}}</p>
+                    <p class="card-text"><strong>Date:</strong> {{$movie->date}}</p>
+                    <p class="card-text"><strong>Vote:</strong> {{$movie->vote}}</p>
                 </div>
             </div>
         </div>
         @empty
         <div class="col">
-            <p>No movies here, sorry 😢</p>
+            <div class="alert alert-warning" role="alert">
+                No movies found. 😢
+            </div>
         </div>
         @endforelse
-
+    </div>
+</div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-          <a class="navbar-brand" href="#">Movies Database</a>
+          <a class="navbar-brand" href="">Movies Database</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -9,10 +9,10 @@
           <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link " href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Movies</a>
+                <a class="nav-link {{ Route::currentRouteName() === 'guests.movies' ? 'active' : ''}}" href="{{route('guests.movies')}}" href="#">Movies</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Categories</a>
